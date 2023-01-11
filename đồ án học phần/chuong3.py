7cf817da631338902614272fe5cfb11871be863b
trongnghia750 <117985171+trongnghia750@users.noreply.github.com>
2023-01-11 16:24:31 +0700

    Add files via upload

---
+++ đồ án học phần/chuong3.py
@@ -0,0 +1,40 @@
+import pickle
+from staff import NhanVien
+
+def nhap_list_danh_sach_nhan_vien(ds_nhan_vien):
+    while True:
+        hoten = input("Nhập họ tên nhân viên (nhập 'q' để dừng nhập): ")
+        if hoten == 'q':
+            break
+        tuoi = int(input("Nhập tuổi nhân viên = "))
+        luong = input("Nhập lương nhân viên = ")
+        nhan_vien = NhanVien(hoten, tuoi, luong)
+        ds_nhan_vien.append(nhan_vien)
+    return ds_nhan_vien
+def in_list_danh_sach_nhan_vien(ds_nhan_vien):
+    for item in ds_nhan_vien:
+        print("Danh sách nhân viên :" , item)
+    return item
+def sap_xep_danh_sach(ds_nhan_vien):
+    ds_sinh_vien = sorted(ds_nhan_vien, reverse=True)
+    for item in ds_sinh_vien:
+        print("Sắp xếp danh sách: " ,item)
+    return item
+def luu_tap_tinnhiphan(ds_nhan_vien, filename):
+    with open(filename,'wb') as f:
+        pickle.dump(ds_nhan_vien, f)
+        print("Lưu tập tin nhị phân")
+def doc_tap_tin_nhi_phan(filename):
+    with open(filename, 'rb') as f:
+        a = f.read()
+        print('Đọc tập tin nhị phân', a)
+        f.close()
+def main():
+    ds_nhan_vien = []
+    nhap_list_danh_sach_nhan_vien(ds_nhan_vien)
+    in_list_danh_sach_nhan_vien(ds_nhan_vien)
+    sap_xep_danh_sach(ds_nhan_vien)
+    luu_tap_tinnhiphan(ds_nhan_vien, 'NhanVien.txt')
+    doc_tap_tin_nhi_phan('NhanVien.txt')
+if __name__=='__main__':
+    main()
